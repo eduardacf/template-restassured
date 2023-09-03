daae817f43580e397fa1528bf3b70416cabbc969
duda <eduarda_26_duda@hotmail.com>
2023-09-03 19:53:21 -0300

    .

---
+++ src/main/java/br/eduardacf/template/constants/Gerais.java
@@ -0,0 +1,10 @@
+package br.eduardacf.template.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Gerais {
+    public static final String BASE_URL = "base_url";
+    public static final String CAMINHO_PROPRIEDADES = "src/main/resources/application.properties";
+}
