72e903c7d4666f8d9778c17f93649021280d001e
duda <eduarda_26_duda@hotmail.com>
2023-09-09 15:43:19 -0300

    .

---
+++ src/main/java/br/eduardacf/template/constants/Mensagens.java
@@ -0,0 +1,11 @@
+package br.eduardacf.template.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Mensagens {
+    public static final String MSG_INVALID_CREDENTIALS = "Invalid credentials";
+    public static final String MSG_PRODUTO_NAO_ENCONTRADO = "Product with id '11111111' not found";
+    public static final String MESSAGE = "message";
+}
---
+++ src/main/java/br/eduardacf/template/constants/auth/Auth.java
@@ -0,0 +1,12 @@
+package br.eduardacf.template.constants.auth;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Auth {
+    public static final String USERNAME_VALIDO = "kminchelle";
+    public static final String EMAIL_VALIDO = "kminchelle@qq.com";
+    public static final String SENHA_VALIDA = "0lelplR";
+    public static final String SENHA_INVALIDA = "0lelplXXXXXXXXX";
+}
